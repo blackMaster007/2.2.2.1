bd940247247bc8ef419c12309d0fc9c2f76b0af2
blackMaster007 <vturchkov@yandex.ru>
2023-05-03 14:57:18 +0300

    done

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -1,10 +1,12 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import javax.persistence.NoResultException;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -15,10 +17,16 @@ public class MainApp {
 
       UserService userService = context.getBean(UserService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+      Car car1 = new Car("Ваз", 2110);
+      Car car2 = new Car("BMV", 3);
+      Car car3 = new Car("Газ", 56);
+      Car car4 = new Car("Пежо", 308);
+
+      userService.add(new User("User1", "Lastname1", "user1@mail.ru", car1));
+      userService.add(new User("User2", "Lastname2", "user2@mail.ru", car2));
+      userService.add(new User("User3", "Lastname3", "user3@mail.ru", car3));
+      userService.add(new User("User4", "Lastname4", "user4@mail.ru", car4));
+
 
       List<User> users = userService.listUsers();
       for (User user : users) {
@@ -26,7 +34,12 @@ public class MainApp {
          System.out.println("First Name = "+user.getFirstName());
          System.out.println("Last Name = "+user.getLastName());
          System.out.println("Email = "+user.getEmail());
-         System.out.println();
+         System.out.println("Car = "+user.getCar().getModel() + " " + user.getCar().getSeries());
+      }
+      try {
+         System.out.println(userService.getUserByCar("Пежо", 308));
+      } catch (NoResultException e) {
+         System.out.println("User по Вашему запросу не найден");
       }
 
       context.close();
